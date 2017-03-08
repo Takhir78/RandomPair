79a03dd46ab375f773c004d4f4ea48ec1e7ae295
Takhir78 <t.salimov@gmail.com>
2017-03-08 00:52:23 -0500

    Classmates

---
+++ random_pairs.rb
@@ -0,0 +1,33 @@
+def random(array)
+	array.count
+end
+
+def rpairs(array)
+	array
+end
+
+def shuffled_names(array)
+	shuffled = array.shuffle.each_slice(2).to_a
+	shuffled.count	
+	# shuffled	
+end  #works for even number of strings in an array but not odd
+array = ["Mike","Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takhir"]
+# puts shuffled_names(array)
+
+
+def shuffled_names_odd(array)  #Trying to figure out how to add odd man out into a pairing
+ 	shuffled = array.shuffle.each_slice(2).to_a
+	shuffled.each do |pair|
+		if pair.count == 1
+	 		shuffled[0].push(pair[0])
+			shuffled.pop
+		end
+  	end
+	shuffled.count
+	# shuffled
+end
+
+classmates = ["Mike", "Bob", "Joe", "Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takhir"]
+# print shuffled_names_odd(classmates)
+
+
---
+++ test_random_pairs.rb
@@ -0,0 +1,28 @@
+require "minitest/autorun"
+require_relative "random_pairs.rb"
+
+class TestRandomPairs < Minitest::Test
+
+	def test_returns_array
+		array = []
+		assert_equal(0, random(array))
+	end
+
+	def test_returns_populated_array
+		array = ["Mike","Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takhir"]
+		assert_equal(["Mike","Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takhir"], rpairs(array))
+	end
+
+	def test_returns_populated_array_randomized
+		array = ["Mike","Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takhir"]
+		assert_equal(4, shuffled_names(array))
+	end
+
+	def test_returns_populated_array_randomized_adds_extra_to_group
+		array = ["Mike","Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takhir"]
+		assert_equal(3, shuffled_names_odd(array))
+	end
+
+end
+
+
